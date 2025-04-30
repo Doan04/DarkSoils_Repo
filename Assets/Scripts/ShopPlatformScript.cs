d263f71fc3971ef86f3dffe08ec57fd1b94444f9
Woohoolol <137485756+Woohoolol@users.noreply.github.com>
2025-04-30 16:24:04 -0500

    sprucing up the shop

--- Assets/Scripts/ShopPlatformScript.cs
+++ Assets/Scripts/ShopPlatformScript.cs
@@ -13,6 +13,8 @@ public class ShopPlatformScript : MonoBehaviour
     public int price;
     public bool currentlyOnShopPlatform;
     public bool purchasing;
+    public Vector3 originalCoordinatesPopup;
+    public Vector3 originalCoordinatesDescription;
     public GameObject popup;
     public GameObject description;
     public AudioSource[] audioText;
@@ -20,6 +22,8 @@ public class ShopPlatformScript : MonoBehaviour
     {
         popup = gameObject.transform.GetChild(0).gameObject;    
         description = gameObject.transform.GetChild(1).gameObject;
+        originalCoordinatesPopup = popup.transform.position;
+        originalCoordinatesDescription = description.transform.position;
         timesBought = 0;
         currentlyOnShopPlatform = false;
         purchasing = false;
@@ -37,7 +41,20 @@ public class ShopPlatformScript : MonoBehaviour
                 audioText[1].Play();
             }
         }
+        platformRotate();
     }
+
+    public void platformRotate()
+    {
+        float rotationSpeed = 10;
+        transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime);
+        //So that the text boxes don't rotate
+        popup.transform.Rotate(0f, 0f, -rotationSpeed * Time.deltaTime);
+        popup.transform.position = originalCoordinatesPopup;
+        description.transform.Rotate(0f, 0f, -rotationSpeed * Time.deltaTime);
+        description.transform.position = originalCoordinatesDescription;
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         GameObject collidedObject = collision.gameObject;
